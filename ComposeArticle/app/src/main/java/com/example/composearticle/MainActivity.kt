d72a201131583c8c9531ae8010bfc6712e1e9c49
Dhruv Gupta <gupta8737@students.d211.org>
2024-08-22 08:11:19 -0500

    finish compose article

---
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.GreetingText
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeArticleTheme {
+                Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                    GreetingImage("Jetpack Compose tutorial", "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs", "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingImage(text1: String, text2: String, text3: String, modifier: Modifier = Modifier ) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+//            modifier = Modifier.fillMaxSize()
+//                .align(alignment = Alignment.Top)
+                .padding(bottom = 4.dp)
+        )
+//        Spacer(modifier = Modifier.height(30.dp))
+        GreetingText(
+            text1 = text1,
+            text2 = text2,
+            text3 = text3,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+
+@Composable
+fun GreetingText(text1: String, text2: String, text3: String, modifier: Modifier = Modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(
+                text = text1,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+
+            )
+            Text(
+                text = text2,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .padding(end = 16.dp)
+            )
+            Text(
+                text = text3,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
+            )
+        }
+}
+@Preview(showBackground = true)
+@Composable
+fun ArticlePreview() {
+    ComposeArticleTheme {
+        GreetingImage(
+            text1 = stringResource(R.string.jetpack_compose_tutorial),
+            text2 = stringResource(R.string.jetpack_compose_is_a_modern_toolkit_for_building_native_android_ui_compose_simplifies_and_accelerates_ui_development_on_android_with_less_code_powerful_tools_and_intuitive_kotlin_apis),
+            text3 = stringResource(R.string.in_this_tutorial_you_build_a_simple_ui_component_with_declarative_functions_you_call_compose_functions_to_say_what_elements_you_want_and_the_compose_compiler_does_the_rest_compose_is_built_around_composable_functions_these_functions_let_you_define_your_app_s_ui_programmatically_because_they_let_you_describe_how_it_should_look_and_provide_data_dependencies_rather_than_focus_on_the_process_of_the_ui_s_construction_such_as_initializing_an_element_and_then_attaching_it_to_a_parent_to_create_a_composable_function_you_add_the_composable_annotation_to_the_function_name)
+        )
+    }
+}
