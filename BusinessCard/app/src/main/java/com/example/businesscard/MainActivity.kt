1c96efd9b912e86f1a40926ed56cdee5e0edd701
dhruvg130 <dhruvguptadhruv@yahoo.com>
2024-08-25 21:38:22 -0500

    finished business card app( added icons and images)

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.example.businesscard
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,28 +49,81 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun BusinessCard(text1: String, text2: String, text3: String, text4: String, text5: String, modifier: Modifier = Modifier ) {
-//    val image = painterResource()
+    val image = painterResource(R.drawable.img_3439)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .size(350.dp)
+                .offset(y = 20.dp)
+            //ChatGpt link which was used to help format image
+            // https://chatgpt.com/share/c0d85a34-07f0-4797-a11b-749f86cf2964
+        )
+    }
+    LowerPart(
+        icon = ImageVector,
+        number = text3,
+        socialMedia = text4,
+        email = text5
+    )
+    UpperPart(
+        name = text1,
+        title = text2)
 }
 
 @Composable
-fun LowerPart(number: String, socialMedia: String, email: String, modifier: Modifier = Modifier ){
+fun LowerPart(
+    number: String,
+    socialMedia: String,
+    email: String,
+    modifier: Modifier = Modifier,
+    icon: Any
+){
     Column(
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize ()
+        modifier = Modifier
+            .fillMaxSize()
+            .offset(y = -30.dp)
     ) {
-        Text(
-            text = number,
-            fontSize = 30.sp,
-        )
-        Text(
-            text = socialMedia,
-            fontSize = 30.sp
-        )
-        Text(
-            text = email,
-            fontSize = 30.sp
-        )
+        Row {
+            Icon(
+                imageVector = Icons.Filled.Phone,
+                contentDescription = "Phone"
+            )
+            Text(
+                text = number,
+                fontSize = 22.sp,
+            )
+        }
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.instagram),
+                contentDescription = null,
+                modifier = Modifier.size(35.dp)
+            )
+            Text(
+                text = socialMedia,
+                fontSize = 22.sp
+            )
+        }
+        Row {
+            Icon(
+                imageVector = Icons.Filled.MailOutline,
+                contentDescription = "Phone",
+                modifier = Modifier
+                    .size(30.dp)
+            )
+            Text(
+                text = email,
+                fontSize = 22.sp
+            )
+
+        }
     }
 }
 
@@ -90,13 +151,12 @@ fun UpperPart(name: String, title: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        UpperPart(
-            name = "Dhruv",
-            title = "Student"
+        BusinessCard(
+            text1 = stringResource(R.string.name) ,
+            text2 = stringResource(R.string.position),
+            text3 = stringResource(R.string.phone_number) ,
+            text4 = stringResource(R.string.instagram),
+            text5 = stringResource(R.string.email)
         )
-        LowerPart(
-            number = "11111",
-            socialMedia = "hihh" ,
-            email = "hhh")
     }
 }
