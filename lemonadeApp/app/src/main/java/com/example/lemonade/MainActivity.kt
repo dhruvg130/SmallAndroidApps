584a70f239910b3008973ccd302b304fb1275c0b
Dhruv Gupta <gupta8737@students.d211.org>
2024-09-05 08:16:35 -0500

    started lemonade app

--- lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -19,33 +24,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Display(
-    contentDescription: Int,
-
+fun Screen(
     modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+){
+    Column {
+        Row {
+            Text(
+                text = stringResource(id = R.string.app_name)
+            )
+        }
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.lemon_tree),
+                contentDescription = stringResource(id = R.string.tree)
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LemonadeTheme {
-        Greeting("Android")
+        }
     }
 }
