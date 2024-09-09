85d92224fd7521d59a78170ca5aa9e4323b27c4f
Dhruv Gupta <gupta8737@students.d211.org>
2024-09-09 08:20:05 -0500

    changed the functionality of Lemonade project

--- lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.example.lemonade
 
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -46,41 +47,60 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LemonPreview(){
     var screen by remember { mutableStateOf(1)}
+    var painter : Int
+    var sentence: String
+    var clickCount by remember { mutableStateOf(1)}
+    var onClick: () -> Unit
     when(screen){
-        1 -> Screen(
-            painter = painterResource(id = R.drawable.lemon_tree),
-            description = stringResource(id = R.string.tree),
-            modifier = Modifier
-                .clickable { screen = 2 }
-        )
-        2 -> Screen(
-            painter = painterResource(id = R.drawable.lemon_squeeze),
-            description = stringResource(id = R.string.lemon),
-            modifier = Modifier
-                .clickable { screen = 3 }
-        )
-        3 -> Screen(
-            painter = painterResource(id = R.drawable.lemon_drink),
-            description = stringResource(id = R.string.tap),
-            modifier = Modifier
-                .clickable { screen = 4 }
-        )
-        4 -> Screen(
-            painter = painterResource(id = R.drawable.lemon_restart),
-            description = stringResource(id = R.string.retap),
-            modifier = Modifier
-                .clickable { screen = 1 }
-        )
-        else -> Text(text = "error")
+        1 -> {
+            painter = R.drawable.lemon_tree
+            sentence= stringResource(id = R.string.tap)
+            onClick = {
+                screen = 2
+                clickCount = (2..4).random()
+            }
+        }
+        2 -> {
+            painter = R.drawable.lemon_squeeze
+            sentence = stringResource(id = R.string.keep_going)
+            onClick = {
+                clickCount--
+                if (clickCount == 0) {
+                    screen = 3
+                }
+
+            }
+        }
+        3 -> {
+            painter = R.drawable.lemon_drink
+            sentence = stringResource(id = R.string.drink)
+            onClick = {
+                screen = 4
+            }
+        }
+        else -> {
+            painter = R.drawable.lemon_restart
+            sentence = stringResource(id = R.string.retap)
+            onClick = {
+                screen = 1
+            }
+        }
+//        else -> Text(text = "error")
     }
+    Screen(
+        description = sentence,
+        image = painter,
+        clickImage = onClick
+    )
 }
 
 
 @Composable
 fun Screen(
-    painter: Painter,
     description: String,
-    modifier: Modifier = Modifier
+    image: Int,
+    clickImage: () -> Unit,
+    modifier: Modifier = Modifier,
 ){
     Column {
         Row {
@@ -90,13 +110,19 @@ fun Screen(
             )
         }
         Spacer(modifier = Modifier.height(32.dp))
-        Column {
+        Button(
+            onClick = clickImage,
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = Color.LightGray
+            ),
+            shape = RoundedCornerShape(100.dp)
+        ) {
             Image(
-                painter = painter,
+                painter = painterResource(id = image),
                 contentDescription = description,
                 modifier = modifier
-
             )
         }
+        Text(text = description)
     }
 }
