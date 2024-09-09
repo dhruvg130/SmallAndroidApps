3d9e43a444d4bd94329fae6ded922048d8796f48
dhruvg130 <dhruvguptadhruv@yahoo.com>
2024-09-09 18:45:24 -0500

    fixed coloring and alignments of texts

--- lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ lemonadeApp/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,11 +25,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,23 +43,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-
+                LemonPreview()
             }
         }
     }
 }
 
-@Preview( showBackground = true )
+@Preview( showSystemUi = true )
 @Composable
 fun LemonPreview(){
     var screen by remember { mutableStateOf(1)}
-    var painter : Int
-    var sentence: String
+    val description: String
+    val painter : Int
+    val sentence: String
     var clickCount by remember { mutableStateOf(1)}
-    var onClick: () -> Unit
+    val onClick: () -> Unit
     when(screen){
         1 -> {
             painter = R.drawable.lemon_tree
+            description = stringResource(id = R.string.tree)
             sentence= stringResource(id = R.string.tap)
             onClick = {
                 screen = 2
@@ -62,6 +70,7 @@ fun LemonPreview(){
         }
         2 -> {
             painter = R.drawable.lemon_squeeze
+            description = stringResource(id = R.string.lemon)
             sentence = stringResource(id = R.string.keep_going)
             onClick = {
                 clickCount--
@@ -73,6 +82,7 @@ fun LemonPreview(){
         }
         3 -> {
             painter = R.drawable.lemon_drink
+            description = stringResource(id = R.string.glass)
             sentence = stringResource(id = R.string.drink)
             onClick = {
                 screen = 4
@@ -80,6 +90,7 @@ fun LemonPreview(){
         }
         else -> {
             painter = R.drawable.lemon_restart
+            description = stringResource(id = R.string.e_glass)
             sentence = stringResource(id = R.string.retap)
             onClick = {
                 screen = 1
@@ -88,8 +99,9 @@ fun LemonPreview(){
 //        else -> Text(text = "error")
     }
     Screen(
-        description = sentence,
+        description = description,
         image = painter,
+        sentence = sentence,
         clickImage = onClick
     )
 }
@@ -98,24 +110,33 @@ fun LemonPreview(){
 @Composable
 fun Screen(
     description: String,
+    sentence: String,
     image: Int,
     clickImage: () -> Unit,
     modifier: Modifier = Modifier,
 ){
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         Row {
             Text(
                 text = stringResource(id = R.string.app_name),
-                fontSize = 18.sp,
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(red = 196, green = 182, blue = 61)),
+                textAlign = TextAlign.Center
+
             )
         }
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(200.dp))
         Button(
             onClick = clickImage,
             colors = ButtonDefaults.textButtonColors(
                 containerColor = Color.LightGray
             ),
-            shape = RoundedCornerShape(100.dp)
+            shape = RoundedCornerShape(80.dp)
         ) {
             Image(
                 painter = painterResource(id = image),
@@ -123,6 +144,9 @@ fun Screen(
                 modifier = modifier
             )
         }
-        Text(text = description)
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = sentence
+        )
     }
 }
