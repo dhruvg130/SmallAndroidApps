1264e7e5a0ee9e9c8d85a5a0bb603630cfe2d65c
Dhruv Gupta <gupta8737@students.d211.org>
2024-09-11 08:17:28 -0500

    added keyboard action to create custom tip

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,14 +2,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,8 +52,11 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
-         Column(
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent)
+
+    Column(
             modifier = Modifier
                 .statusBarsPadding()
                 .padding(horizontal = 40.dp)
@@ -64,8 +71,27 @@ fun TipTimeLayout() {
                     .align(alignment = Alignment.Start)
             )
              EditNumberField(
+                 label = R.string.bill_amount,
+                 keyboardOptions = KeyboardOptions.Default.copy(
+                     keyboardType = KeyboardType.Number,
+                     imeAction = ImeAction.Next
+                 ),
                  value = amountInput,
-                 onValueChange = { amountInput = it },
+                 onValueChange = { amountInput = it }, // when number is inputted it changes amountInput
+                 modifier = Modifier
+                     .padding(bottom = 32.dp)
+                     .fillMaxWidth()
+             )
+
+
+             EditNumberField(
+                 label = R.string.how_was_the_service,
+                 keyboardOptions = KeyboardOptions.Default.copy(
+                     keyboardType = KeyboardType.Number,
+                     imeAction = ImeAction.Done
+                 ),
+                 value = tipInput,
+                 onValueChange = {tipInput = it },
                  modifier = Modifier
                      .padding(bottom = 32.dp)
                      .fillMaxWidth()
@@ -78,7 +104,16 @@ fun TipTimeLayout() {
         }
     }
 
-
+@Composable
+fun RoundTheTipRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(40.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+    }
+}
 
 /**
  * Calculates the tip based on the user input and format the tip amount
@@ -92,17 +127,21 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     TextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        label = { Text(stringResource(R.string.bill_amount)) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = { Text(stringResource(label)) },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+        ),
         modifier = modifier
     )
 }
