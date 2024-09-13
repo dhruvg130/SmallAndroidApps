6e804e9ec38517860b65abb906aa700b823c6789
Dhruv Gupta <gupta8737@students.d211.org>
2024-09-13 08:11:57 -0500

    added the ui and need to finish the string array

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.artspace
 
+import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,4 +36,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+@Composable
+fun Picture(
+    childName: Int,
+//    author: String, do later
+    image: Int,
+    nextImage: () -> Unit,
+    previousImage: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+                painter = painterResource(id = image),
+                contentDescription = "Children"
+            )
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(
+                text = stringResource(id = childName)
+            )
+        
+        Spacer(modifier = Modifier.height(50.dp))
+        Row {
+            Button(onClick = previousImage ) {
+                
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(onClick = { /*TODO*/ }) {
 
+            }
+        }
+    }
+}
+
+fun changePic
