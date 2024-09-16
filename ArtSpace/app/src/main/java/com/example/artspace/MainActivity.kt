d8c51a7e31188680d314ac682ec2b45b36ab4f79
Dhruv Gupta <gupta8737@students.d211.org>
2024-09-16 08:20:25 -0500

    worked ArtSpace app need to work on the display

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -16,9 +16,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,41 +41,67 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 @Composable
-fun Picture(
-    childName: Int,
-//    author: String, do later
-    image: Int,
-    nextImage: () -> Unit,
-    previousImage: () -> Unit,
-    modifier: Modifier = Modifier
+@Preview(showSystemUi = true)
+fun Display(
 ) {
+
+    val images = listOf(
+        R.drawable.kabir,
+        R.drawable.khushi,
+        R.drawable.me,
+        R.drawable.paramatel,
+        R.drawable.stanley,
+        R.drawable.tyler,
+        )
+
+    var currentImageIndex by remember { mutableStateOf(0) }
     Column(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-                painter = painterResource(id = image),
+                painter = painterResource(id = images[currentImageIndex]),
                 contentDescription = "Children"
             )
         Spacer(modifier = Modifier.height(100.dp))
         Text(
-                text = stringResource(id = childName)
+                text = stringArrayResource(id = children_names[currentImageIndex])
             )
-        
         Spacer(modifier = Modifier.height(50.dp))
-        Row {
-            Button(onClick = previousImage ) {
-                
+        ChangePic(
+            previousImage = {
+                currentImageIndex = if (currentImageIndex > 0) {
+                    currentImageIndex - 1
+                } else {
+                    images.size - 1
+                }
+            },
+            nextImage = {
+                currentImageIndex = if (currentImageIndex < images.size - 1) {
+                    currentImageIndex + 1
+                } else {
+                    0
+                }
             }
-            Spacer(modifier = Modifier.height(50.dp))
-            Button(onClick = { /*TODO*/ }) {
-
-            }
+        )
+    }
+}
+@Composable
+fun ChangePic(
+    previousImage: () -> Unit,
+    nextImage: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    var screenNumber by remember { mutableStateOf(1) }
+    Row {
+        Button(onClick = previousImage ) {
+            Text("Previous")
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(onClick = nextImage ) {
+            Text("Next")
         }
     }
 }
-
-fun changePic
